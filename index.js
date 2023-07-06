63ef5cefd8b3b6903922eacf76d5d0b1446374cb
paresiqbal <thecrowdel@gmail.com>
2023-07-06 15:02:55 +0700

    Ganjil genap is done

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Even and Odd number
 
+let genap = [];
+let ganjil = [];
+
 for (let i = 1; i <= 100; i++) {
   if (i % 2 == 0) {
-    console.log("genap");
+    genap = [...genap, i];
   } else {
-    console.log("ganjil");
+    ganjil = [...ganjil, i];
   }
 }
+
+console.log({ ganjil, genap });
