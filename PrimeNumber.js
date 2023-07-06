6bf59b239ed99ef4559b1082ea252d49bf180326
paresiqbal <thecrowdel@gmail.com>
2023-07-06 15:42:02 +0700

    Add prime number

---
+++ PrimeNumber.js
@@ -0,0 +1,18 @@
+// Find prime number
+let prima = [];
+
+for (let i = 2; i <= 100; i++) {
+  let bilangan = 0;
+
+  for (let a = 1; a <= i; a++) {
+    if (i % a == 0) {
+      bilangan = bilangan + 1;
+    }
+  }
+
+  if (bilangan == 2) {
+    prima = [...prima, i];
+  }
+}
+
+console.log(prima);
